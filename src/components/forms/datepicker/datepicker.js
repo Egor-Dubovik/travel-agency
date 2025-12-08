87eec2717d0a90ecc26102d711857e02facdd75e
unknown <egorka54896@gmail.com>
2025-12-08 21:41:52 +0300

    feat: add first-screan for home page

--- src/components/forms/datepicker/datepicker.js
+++ src/components/forms/datepicker/datepicker.js
@@ -1,22 +1,26 @@
-// Підключення модуля
 import datepicker from 'js-datepicker';
-
 import langs from './_lang.json';
 import './datepicker.scss';
 
 if (document.querySelector('[data-fls-datepicker]')) {
 	const LANG = 'en'; // ua
-	const datePicker = datepicker('[data-fls-datepicker]', {
-		customDays: langs[LANG].week,
-		customMonths: langs[LANG].month,
-		overlayButton: langs[LANG].button,
-		overlayPlaceholder: langs[LANG].year,
-		startDay: 0,
-		formatter: (input, date, instance) => {
-			const value = date.toLocaleDateString();
-			input.value = value;
-		},
-		onSelect: function (input, instance, date) {},
+	const datepickers = document.querySelectorAll('[data-fls-datepicker]');
+
+	datepickers.forEach(el => {
+		const datePicker = datepicker(el, {
+			customDays: langs[LANG].week,
+			customMonths: langs[LANG].month,
+			overlayButton: langs[LANG].button,
+			overlayPlaceholder: langs[LANG].year,
+			startDay: 0,
+			formatter: (input, date, instance) => {
+				const value = date.toLocaleDateString();
+				input.value = value;
+			},
+			onSelect: function (input, instance, date) {},
+		});
+
+		window.flsDatepicker = window.flsDatepicker || [];
+		window.flsDatepicker.push(datePicker);
 	});
-	window.flsDatepicker = datePicker;
 }
